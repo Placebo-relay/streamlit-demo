fda127fd5e8fe2fc38b2cfa6b36d50572d9ee575
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2023-10-17 04:19:35 -0900

    Update streamlit_app.py

--- p3/streamlit_app.py
+++ p3/streamlit_app.py
@@ -40,6 +40,7 @@ def main():
         num_lines = st.sidebar.slider("Number of Lines", 5, 10, 5)
         generate_random_file(file_path, num_lines)
         st.success(f"Generated random file with {num_lines} lines.")
+        st.empty()
 
     elif option == "Upload File":
         uploaded_file = st.sidebar.file_uploader("Upload a file")
@@ -48,6 +49,7 @@ def main():
             with open(file_path, 'wb') as file:
                 file.write(file_contents)
             st.success("Uploaded file successfully.")
+            st.empty()
 
     display_file_contents(file_path)
 
@@ -63,12 +65,13 @@ def main():
         else:
             relocate_line(file_path, modified_file_path, n, m)
             st.success(f"Line {n} relocated after Line {m}.")
+            st.empty()
             display_modified_file_contents(modified_file_path)
 
-        if st.button("Prepare file for download"):
-            with open(modified_file_path, 'r') as file:
-                modified_file_contents = file.read()
-            st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
+    if st.button("Prepare file for download"):
+        with open(modified_file_path, 'r') as file:
+            modified_file_contents = file.read()
+        st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
 
 if __name__ == "__main__":
     main()
