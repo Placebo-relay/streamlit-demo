30575dc61ce03472d041b9c072eb1d05b0a34c70
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2023-10-17 04:59:06 -0900

    Update streamlit_app.py

--- p3/streamlit_app.py
+++ p3/streamlit_app.py
@@ -1,83 +1,89 @@
 import streamlit as st
 from streamlit import file_uploader
 import random
-from translate import Translator
 
-def translate_text(text, target_language):
-    translator = Translator(to_lang=target_language)
-    translation = translator.translate(text)
-    return translation
+def generate_random_file(file_path, num_lines):
+    with open(file_path, 'w') as file:
+        for i in range(num_lines):
+            file.write(f'Line {i+1}\n')
 
-# Translate the text strings
-st.title(translate_text("Line Relocator", "ja"))
+def relocate_line(file_path, modified_file_path, n, m, log_file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
-st.sidebar.title(translate_text("Options", "ja"))
-option = st.sidebar.radio(translate_text("Select an option:", "ja"), (
-    translate_text("Generate Random File", "ja"),
-    translate_text("Upload File", "ja")
-))
+    line_n = lines.pop(n-1)
+    lines.insert(m, line_n)
 
-file_path = 'original_file.txt'
-modified_file_path = 'file_modified.txt'
-log_file_path = 'relocation_log.txt'
+    with open(modified_file_path, 'w') as file:
+        file.writelines(lines)
 
-if option == translate_text("Generate Random File", "ja"):
-    num_lines = st.sidebar.slider(translate_text("Number of Lines", "ja"), 5, 10, 5)
-    generate_random_file(file_path, num_lines)
-    st.success(translate_text(f"Generated random file with {num_lines} lines.", "ja"))
-    with open(log_file_path, 'w') as file:
-        file.write(translate_text("Relocation Log:\n", "ja"))
+    with open(log_file_path, 'a') as file:
+        file.write(f"Line {n} relocated after Line {m}\n")
 
-elif option == translate_text("Upload File", "ja"):
-    uploaded_file = st.sidebar.file_uploader(translate_text("Upload a file", "ja"))
-    if uploaded_file is not None:
-        file_contents = uploaded_file.read()
-        with open(file_path, 'wb') as file:
-            file.write(file_contents)
-        st.success(translate_text("Uploaded file successfully.", "ja"))
-        with open(log_file_path, 'w') as file:
-            file.write(translate_text("Relocation Log:\n", "ja"))
+def display_file_contents(file_path):
+    with open(file_path, 'r') as file:
+        contents = file.read()
+    st.text_area("Original File Contents", value=contents, height=200)
 
-display_file_contents(file_path)
-
-num_lines = sum(1 for line in open(file_path))
-n = st.number_input(translate_text("Line n:", "ja"), min_value=1, max_value=num_lines, value=1, step=1)
-m = st.number_input(translate_text("Line m:", "ja"), min_value=1, max_value=num_lines, value=1, step=1)
-
-if st.button(translate_text("Relocate", "ja")):
-    if n == m:
-        st.error(translate_text("Error: Line n and Line m must have different values.", "ja"))
-    elif n - 1 == m:
-        st.error(translate_text("Error: Line n is already after Line m.", "ja"))
-    else:
-        relocate_line(file_path, modified_file_path, n, m, log_file_path)
-        st.success(translate_text(f"Line {n} relocated after Line {m}.", "ja"))
-        display_modified_file_contents(modified_file_path)
-
-if st.button(translate_text("Prepare file for download", "ja")):
+def display_modified_file_contents(modified_file_path):
     with open(modified_file_path, 'r') as file:
-        modified_file_contents = file.read()
-    st.download_button(translate_text("Download", "ja"), data=modified_file_contents, file_name=modified_file_path)
+        contents = file.read()
+    st.text_area("Modified File Contents", value=contents, height=200)
 
-display_relocation_log(log_file_path)
+def display_relocation_log(log_file_path):
+    with open(log_file_path, 'r') as file:
+        contents = file.read()
+    st.sidebar.text_area("Relocation Log", value=contents, height=200)
 
-# Add language options
-language_options = {
-    "Japanese": "ja",
-    "Russian": "ru",
-    "German": "de",
-    "French": "fr"
-}
+def main():
+    st.title("Line Relocator")
 
-selected_language = st.sidebar.selectbox("Select Language", list(language_options.keys()))
+    st.sidebar.title("Options")
+    option = st.sidebar.radio("Select an option:", ("Generate Random File", "Upload File"))
 
-if st.sidebar.button("Translate"):
-    target_language = language_options[selected_language]
-    st.title(translate_text("Line Relocator", target_language))
-    st.sidebar.title(translate_text("Options", target_language))
-    option = st.sidebar.radio(translate_text("Select an option:", target_language), (
-        translate_text("Generate Random File", target_language),
-        translate_text("Upload File", target_language)
-    ))
+    file_path = 'original_file.txt'
+    modified_file_path = 'file_modified.txt'
+    log_file_path = 'relocation_log.txt'
 
-    # Rest of the code remains the same
+    if option == "Generate Random File":
+        num_lines = st.sidebar.slider("Number of Lines", 5, 10, 5)
+        generate_random_file(file_path, num_lines)
+        st.success(f"Generated random file with {num_lines} lines.")
+        with open(log_file_path, 'w') as file:
+            file.write("Relocation Log:\n")
+
+    elif option == "Upload File":
+        uploaded_file = st.sidebar.file_uploader("Upload a file")
+        if uploaded_file is not None:
+            file_contents = uploaded_file.read()
+            with open(file_path, 'wb') as file:
+                file.write(file_contents)
+            st.success("Uploaded file successfully.")
+            with open(log_file_path, 'w') as file:
+                file.write("Relocation Log:\n")
+
+    display_file_contents(file_path)
+
+    num_lines = sum(1 for line in open(file_path))
+    n = st.number_input("Line n:", min_value=1, max_value=num_lines, value=1, step=1)
+    m = st.number_input("Line m:", min_value=1, max_value=num_lines, value=1, step=1)
+
+    if st.button("Relocate"):
+        if n == m:
+            st.error("Error: Line n and Line m must have different values.")
+        elif n - 1 == m:
+            st.error("Error: Line n is already after Line m.")
+        else:
+            relocate_line(file_path, modified_file_path, n, m, log_file_path)
+            st.success(f"Line {n} relocated after Line {m}.")
+            display_modified_file_contents(modified_file_path)
+
+    if st.button("Prepare file for download"):
+        with open(modified_file_path, 'r') as file:
+            modified_file_contents = file.read()
+        st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
+
+    display_relocation_log(log_file_path)
+
+if __name__ == "__main__":
+    main()
