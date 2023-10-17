cf04fd78523389530ea07c0b3996835d99528765
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2023-10-17 04:27:50 -0900

    Update streamlit_app.py

--- p3/streamlit_app.py
+++ p3/streamlit_app.py
@@ -36,10 +36,12 @@ def main():
     file_path = 'original_file.txt'
     modified_file_path = 'file_modified.txt'
 
+    changes = []
+
     if option == "Generate Random File":
         num_lines = st.sidebar.slider("Number of Lines", 5, 10, 5)
         generate_random_file(file_path, num_lines)
-        st.success(f"Generated random file with {num_lines} lines.")
+        changes.append(f"Generated random file with {num_lines} lines.")
 
     elif option == "Upload File":
         uploaded_file = st.sidebar.file_uploader("Upload a file")
@@ -47,7 +49,10 @@ def main():
             file_contents = uploaded_file.read()
             with open(file_path, 'wb') as file:
                 file.write(file_contents)
-            st.success("Uploaded file successfully.")
+            changes.append("Uploaded file successfully.")
+
+    for change in changes:
+        st.sidebar.write(change)
 
     display_file_contents(file_path)
 
@@ -62,13 +67,13 @@ def main():
             st.error("Error: Line n is already after Line m.")
         else:
             relocate_line(file_path, modified_file_path, n, m)
-            st.success(f"Line {n} relocated after Line {m}.")
+            changes.append(f"Line {n} relocated after Line {m}.")
             display_modified_file_contents(modified_file_path)
 
-        if st.button("Prepare file for download"):
-            with open(modified_file_path, 'r') as file:
-                modified_file_contents = file.read()
-            st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
+    if st.button("Prepare file for download"):
+        with open(modified_file_path, 'r') as file:
+            modified_file_contents = file.read()
+        st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
 
 if __name__ == "__main__":
     main()
