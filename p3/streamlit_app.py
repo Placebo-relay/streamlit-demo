0d3cafae88b11fbc03ea82000d5ad0283e8d3d21
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2023-10-17 03:54:08 -0900

    Update streamlit_app.py

--- p3/streamlit_app.py
+++ p3/streamlit_app.py
@@ -20,7 +20,7 @@ def relocate_line(file_path, modified_file_path, n, m):
 def display_file_contents(file_path):
     with open(file_path, 'r') as file:
         contents = file.read()
-    st.text_area("File Contents", value=contents, height=200)
+    st.text_area("Original File Contents", value=contents, height=200)
 
 def display_modified_file_contents(modified_file_path):
     with open(modified_file_path, 'r') as file:
@@ -39,7 +39,7 @@ def main():
     if option == "Generate Random File":
         num_lines = st.sidebar.slider("Number of Lines", 5, 10, 5)
         generate_random_file(file_path, num_lines)
-        st.success(f"Generated random file with {num_lines} lines.")
+        st.sidebar.success(f"Generated random file with {num_lines} lines.")
 
     elif option == "Upload File":
         uploaded_file = st.sidebar.file_uploader("Upload a file")
@@ -47,7 +47,7 @@ def main():
             file_contents = uploaded_file.read()
             with open(file_path, 'wb') as file:
                 file.write(file_contents)
-            st.success("Uploaded file successfully.")
+            st.sidebar.success("Uploaded file successfully.")
 
     display_file_contents(file_path)
 
@@ -57,18 +57,18 @@ def main():
 
     if st.button("Relocate"):
         if n == m:
-            st.error("Error: Line n and Line m must have different values.")
+            st.sidebar.error("Error: Line n and Line m must have different values.")
         elif n - 1 == m:
-            st.error("Error: Line n is already after Line m.")
+            st.sidebar.error("Error: Line n is already after Line m.")
         else:
             relocate_line(file_path, modified_file_path, n, m)
-            st.success(f"Line {n} relocated after Line {m}.")
+            st.sidebar.success(f"Line {n} relocated after Line {m}.")
             display_modified_file_contents(modified_file_path)
 
-        if st.button("Prepare file for download"):
-            with open(modified_file_path, 'r') as file:
-                modified_file_contents = file.read()
-            st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
+    if st.button("Prepare file for download"):
+        with open(modified_file_path, 'r') as file:
+            modified_file_contents = file.read()
+        st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
 
 if __name__ == "__main__":
     main()
