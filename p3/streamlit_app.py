41f2d6e90d618dae5ea529f4af3023c4c7a1dc95
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2023-10-17 03:57:59 -0900

    Update streamlit_app.py

--- p3/streamlit_app.py
+++ p3/streamlit_app.py
@@ -39,7 +39,7 @@ def main():
     if option == "Generate Random File":
         num_lines = st.sidebar.slider("Number of Lines", 5, 10, 5)
         generate_random_file(file_path, num_lines)
-        st.sidebar.success(f"Generated random file with {num_lines} lines.")
+        st.success(f"Generated random file with {num_lines} lines.")
 
     elif option == "Upload File":
         uploaded_file = st.sidebar.file_uploader("Upload a file")
@@ -47,7 +47,7 @@ def main():
             file_contents = uploaded_file.read()
             with open(file_path, 'wb') as file:
                 file.write(file_contents)
-            st.sidebar.success("Uploaded file successfully.")
+            st.success("Uploaded file successfully.")
 
     display_file_contents(file_path)
 
@@ -55,23 +55,20 @@ def main():
     n = st.number_input("Line n:", min_value=1, max_value=num_lines, value=1, step=1)
     m = st.number_input("Line m:", min_value=1, max_value=num_lines, value=1, step=1)
 
-    relocate_placeholder = st.empty()
-    download_placeholder = st.empty()
-
     if st.button("Relocate"):
         if n == m:
-            relocate_placeholder.error("Error: Line n and Line m must have different values.")
+            st.error("Error: Line n and Line m must have different values.")
         elif n - 1 == m:
-            relocate_placeholder.error("Error: Line n is already after Line m.")
+            st.error("Error: Line n is already after Line m.")
         else:
             relocate_line(file_path, modified_file_path, n, m)
-            relocate_placeholder.success(f"Line {n} relocated after Line {m}.")
+            st.success(f"Line {n} relocated after Line {m}.")
             display_modified_file_contents(modified_file_path)
 
-    if st.button("Prepare file for download"):
-        with open(modified_file_path, 'r') as file:
-            modified_file_contents = file.read()
-        download_placeholder.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
+        if st.button("Prepare file for download"):
+            with open(modified_file_path, 'r') as file:
+                modified_file_contents = file.read()
+            st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
 
 if __name__ == "__main__":
     main()
