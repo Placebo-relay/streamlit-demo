3d5e8af3507fa5bc5a0309cfb6e6d9fee53a9eb8
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2023-10-17 04:31:32 -0900

    Update streamlit_app.py

--- p3/streamlit_app.py
+++ p3/streamlit_app.py
@@ -7,7 +7,7 @@ def generate_random_file(file_path, num_lines):
         for i in range(num_lines):
             file.write(f'Line {i+1}\n')
 
-def relocate_line(file_path, modified_file_path, n, m):
+def relocate_line(file_path, modified_file_path, n, m, log_file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
@@ -16,6 +16,9 @@ def relocate_line(file_path, modified_file_path, n, m):
 
     with open(modified_file_path, 'w') as file:
         file.writelines(lines)
+
+    with open(log_file_path, 'a') as file:
+        file.write(f"Line {n} relocated after Line {m}\n")
 
 def display_file_contents(file_path):
     with open(file_path, 'r') as file:
@@ -27,6 +30,11 @@ def display_modified_file_contents(modified_file_path):
         contents = file.read()
     st.text_area("Modified File Contents", value=contents, height=200)
 
+def display_relocation_log(log_file_path):
+    with open(log_file_path, 'r') as file:
+        contents = file.read()
+    st.sidebar.text_area("Relocation Log", value=contents, height=200)
+
 def main():
     st.title("Line Relocator")
 
@@ -35,13 +43,14 @@ def main():
 
     file_path = 'original_file.txt'
     modified_file_path = 'file_modified.txt'
-
-    changes = []
+    log_file_path = 'relocation_log.txt'
 
     if option == "Generate Random File":
         num_lines = st.sidebar.slider("Number of Lines", 5, 10, 5)
         generate_random_file(file_path, num_lines)
-        changes.append(f"Generated random file with {num_lines} lines.")
+        st.success(f"Generated random file with {num_lines} lines.")
+        with open(log_file_path, 'w') as file:
+            file.write("Relocation Log:\n")
 
     elif option == "Upload File":
         uploaded_file = st.sidebar.file_uploader("Upload a file")
@@ -49,10 +58,9 @@ def main():
             file_contents = uploaded_file.read()
             with open(file_path, 'wb') as file:
                 file.write(file_contents)
-            changes.append("Uploaded file successfully.")
-
-    for change in changes:
-        st.sidebar.write(change)
+            st.success("Uploaded file successfully.")
+            with open(log_file_path, 'w') as file:
+                file.write("Relocation Log:\n")
 
     display_file_contents(file_path)
 
@@ -66,14 +74,16 @@ def main():
         elif n - 1 == m:
             st.error("Error: Line n is already after Line m.")
         else:
-            relocate_line(file_path, modified_file_path, n, m)
-            changes.append(f"Line {n} relocated after Line {m}.")
+            relocate_line(file_path, modified_file_path, n, m, log_file_path)
+            st.success(f"Line {n} relocated after Line {m}.")
             display_modified_file_contents(modified_file_path)
 
     if st.button("Prepare file for download"):
         with open(modified_file_path, 'r') as file:
             modified_file_contents = file.read()
         st.download_button("Download", data=modified_file_contents, file_name=modified_file_path)
+
+    display_relocation_log(log_file_path)
 
 if __name__ == "__main__":
     main()
