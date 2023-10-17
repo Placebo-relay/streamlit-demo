9ce6189c49871d46523e15f82bce66d41ca21a45
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2023-10-17 03:01:20 -0900

    Update streamlit_app.py

--- p3/streamlit_app.py
+++ p3/streamlit_app.py
@@ -2,6 +2,21 @@ import streamlit as st
 from streamlit import file_uploader
 import random
 
+def generate_random_file(num_lines):
+    with open('random_file.txt', 'w') as file:
+        for i in range(num_lines):
+            file.write(f'Line {i+1}\n')
+
+def relocate_line(n, m):
+    with open('random_file.txt', 'r') as file:
+        lines = file.readlines()
+
+    line_n = lines.pop(n-1)
+    lines.insert(m, line_n)
+
+    with open('random_file.txt', 'w') as file:
+        file.writelines(lines)
+
 def display_file_contents():
     with open('random_file.txt', 'r') as file:
         contents = file.read()
@@ -10,7 +25,6 @@ def display_file_contents():
 def main():
     st.title("File Relocator")
 
-    # Create a sidebar with options to generate or upload a file
     st.sidebar.title("Options")
     option = st.sidebar.radio("Select an option:", ("Generate Random File", "Upload File"))
 
@@ -27,15 +41,12 @@ def main():
                 file.write(file_contents)
             st.success("Uploaded file successfully.")
 
-    # Display file contents
     display_file_contents()
 
-    # Create widgets for inputting line numbers
     num_lines = sum(1 for line in open('random_file.txt'))
     n = st.number_input("Line n:", min_value=1, max_value=num_lines, value=1, step=1)
     m = st.number_input("Line m:", min_value=1, max_value=num_lines, value=1, step=1)
 
-    # Button to relocate lines
     if st.button("Relocate"):
         if n == m:
             st.error("Error: Line n and Line m must have different values.")
